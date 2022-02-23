bb8565e3cafbc5f5ef7e0b5e978f42dd6e450a5f
John Law <johnlaw.po@gmail.com>
2022-02-23 01:19:26 +0000

    tests: Add tests for Levenshtein distances

---
+++ tests/distance_damerau.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+
+#include "bktree.hpp"
+
+namespace bk_tree_test {
+
+class Distance_DamerauLevenshtein_TEST : public ::testing::Test {
+protected:
+  Distance_DamerauLevenshtein_TEST() {}
+
+  virtual ~Distance_DamerauLevenshtein_TEST() {}
+
+  virtual void SetUp() {
+    // post-construction
+  }
+
+  virtual void TearDown() {
+    // pre-destruction
+  }
+
+  bk_tree::metrics::DamerauLevenshteinDistance dist;
+};
+
+TEST_F(Distance_DamerauLevenshtein_TEST, DamerauLevenshteinDistances) {
+  EXPECT_TRUE(dist("peter", "piter") == 1);
+  EXPECT_TRUE(dist("peter", "pitar") == 2);
+  EXPECT_TRUE(dist("peter", "pitat") == 3);
+  EXPECT_TRUE(dist("peter", "") == 5);
+  EXPECT_TRUE(dist("", "peter") == 5);
+  EXPECT_TRUE(dist("", "") == 0);
+}
+
+} // namespace bk_tree_test
---
+++ tests/distance_edit.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+
+#include "bktree.hpp"
+
+namespace bk_tree_test {
+
+class Distance_Edit_TEST : public ::testing::Test {
+protected:
+  Distance_Edit_TEST() {}
+
+  virtual ~Distance_Edit_TEST() {}
+
+  virtual void SetUp() {
+    // post-construction
+  }
+
+  virtual void TearDown() {
+    // pre-destruction
+  }
+
+  bk_tree::metrics::EditDistance dist;
+};
+
+TEST_F(Distance_Edit_TEST, EditDistances) {
+  EXPECT_TRUE(dist("peter", "piter") == 1);
+  EXPECT_TRUE(dist("peter", "pitar") == 2);
+  EXPECT_TRUE(dist("peter", "pita") == 3);
+  EXPECT_TRUE(dist("peter", "") == 5);
+  EXPECT_TRUE(dist("", "peter") == 5);
+  EXPECT_TRUE(dist("", "") == 0);
+}
+
+} // namespace bk_tree_test
