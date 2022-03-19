9920aeee0f79c91a2ccb36f928f60d1ed8284209
John Law <johnlaw.po@gmail.com>
2022-03-20 00:20:00 +0800

    chore: change to Wiki's test case

--- tests/bktree_edit.cpp
+++ tests/bktree_edit.cpp
@@ -7,8 +7,8 @@ namespace bk_tree_test {
 class BKTree_Edit_TEST : public ::testing::Test {
 protected:
   BKTree_Edit_TEST() {
-    std::vector<std::string> input{"tall", "tell",  "teel",  "feel", "tally",
-                                   "tuck", "belly", "kelly", "kill", "tal"};
+    std::vector<std::string> input{"book", "books", "cake", "boo", "boon",
+                                   "cook", "cake",  "cape", "cart"};
     for (auto &s : input) {
       tree.insert(s);
     }
@@ -28,15 +28,15 @@ protected:
   bk_tree::ResultList results;
 };
 
-TEST_F(BKTree_Edit_TEST, TreeSize) { EXPECT_EQ(tree.size(), 10); }
+TEST_F(BKTree_Edit_TEST, TreeSize) { EXPECT_EQ(tree.size(), 9); }
 
 TEST_F(BKTree_Edit_TEST, TreeFind) {
-  const std::string &word = "tale";
-  for (int limit = 1; limit <= 3; limit++) {
-    results = tree.find(word, limit);
-    for (auto &p : results) {
-      EXPECT_TRUE(1 <= p.second && p.second <= limit);
-    }
+  const std::string &word = "book";
+  results = tree.find(word, 1);
+  for (auto &p : results) {
+    EXPECT_TRUE(1 >= p.second);
+    EXPECT_TRUE(p.first == "books" || p.first == "boo" || p.first == "boon" ||
+                p.first == "cook" || p.first == "book");
   }
 }
 
