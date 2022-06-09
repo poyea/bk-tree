3c3955a1cb2dd2d34a3252523e694fdab2da4d50
John Law <johnlaw.po@gmail.com>
2022-06-09 02:53:16 +0000

    chore: improve benchmark

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -2,15 +2,26 @@
 
 #include <benchmark/benchmark.h>
 
-void TreeInsert(benchmark::State &state) {
-  bk_tree::BKTree<bk_tree::metrics::EditDistance> tree;
-  bk_tree::ResultList results;
+#include <string_view>
+
+constexpr static const std::string_view word = "word";
+
+void TreeEditInsert(benchmark::State &state) {
+  bk_tree::BKTree<bk_tree::metrics::EditDistance> tree_edit;
   for (auto _ : state) {
-    benchmark::DoNotOptimize(tree.insert("word"));
+    benchmark::DoNotOptimize(tree_edit.insert(word));
   }
 }
 
-BENCHMARK(TreeInsert)->Unit(benchmark::kMicrosecond);
+void TreeHammingInsert(benchmark::State &state) {
+  bk_tree::BKTree<bk_tree::metrics::HammingDistance> tree_hamming;
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(tree_hamming.insert(word));
+  }
+}
+
+BENCHMARK(TreeEditInsert)->Unit(benchmark::kMicrosecond);
+BENCHMARK(TreeHammingInsert)->Unit(benchmark::kMicrosecond);
 
 int main(int argc, char **argv) {
   benchmark::Initialize(&argc, argv);
