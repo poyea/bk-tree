48c6d4df6f1a9938e83eeea18f9d2407c782bd5c
poyea <johnlaw.po@gmail.com>
2022-07-22 15:10:12 +0000

    Deploying to gh-pages from @ poyea/bk-tree@6af361d6585da927789eda10228b072673ffc706 🚀

--- search/all_5.js
+++ search/all_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['lcsdistance_14',['LCSDistance',['../classbk__tree_1_1metrics_1_1LCSDistance.html',1,'bk_tree::metrics']]],
+  ['lcsubseqdistance_14',['LCSubseqDistance',['../classbk__tree_1_1metrics_1_1LCSubseqDistance.html',1,'bk_tree::metrics']]],
   ['leedistance_15',['LeeDistance',['../classbk__tree_1_1metrics_1_1LeeDistance.html',1,'bk_tree::metrics']]]
 ];
--- search/classes_5.js
+++ search/classes_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['lcsdistance_29',['LCSDistance',['../classbk__tree_1_1metrics_1_1LCSDistance.html',1,'bk_tree::metrics']]],
+  ['lcsubseqdistance_29',['LCSubseqDistance',['../classbk__tree_1_1metrics_1_1LCSubseqDistance.html',1,'bk_tree::metrics']]],
   ['leedistance_30',['LeeDistance',['../classbk__tree_1_1metrics_1_1LeeDistance.html',1,'bk_tree::metrics']]]
 ];
